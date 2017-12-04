e2c11f355d5c86ef42e8eec7c4b1c99cf001ff9f
Axel <axel.paris69@gmail.com>
2017-12-04 17:52:04 +0100

    fix bug

--- Outerrain/Include/cameraOrbiter.h
+++ Outerrain/Include/cameraOrbiter.h
@@ -22,7 +22,7 @@ public:
 	Transform Projection(const float width, const float height, const float fov) const;
 	void Frame(const float width, const float height, const float z, const float fov, Vector3& dO, Vector3& dx, Vector3& dy) const;
 	 
-	Vector3 Position();
+	Vector3 Position() const;
 	void SetFrameWidth(float w);
 	void SetFrameHeight(float h);
 	float FrameWidth() const;
