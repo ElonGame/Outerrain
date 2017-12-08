410a2054a94a253c061935216396877a0cc6a0df
Axel <axel.paris69@gmail.com>
2017-12-08 13:49:41 +0100

    Debut intégration gameobject/component

---
+++ Outerrain/Include/component.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+class GameObject;
+
+class Component
+{
+protected:
+	GameObject* gameObject = nullptr;
+
+public:
+	virtual void Start() = 0;
+
+	virtual void OnDestroy() = 0;
+
+	virtual void Update(float dt) = 0;
+
+	void SetGameObject(GameObject* gameobject)
+	{
+		this->gameObject = gameobject;
+	}
+	
+	GameObject* GetGameObject()
+	{
+		return this->gameObject;
+	}
+};
